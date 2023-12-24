814869c73db56051c1d065c0bbcde54ca97249f3
danydodson <danydodson@gmail.com>
2023-12-24 01:54:14 -0600

    :rocket: initial commit

---
+++ src/common/lang/tr-TR.js
@@ -0,0 +1,25 @@
+export default {
+  table: {
+    tips: 'Bu sayfa için {count} sayida betik bulundu',
+    install: 'Yükle',
+    title: 'Başlik',
+    author: 'Yazar',
+    authorTips: '{name} ana sayfasina ulaşmak için tikla',
+    dailyInstalls: 'Günlük',
+    updatedTime: 'Güncellendi',
+    action: 'İşlem',
+    version: 'Sürüm',
+    score: 'Puan',
+    totalInstalls: 'Toplam Yükleme',
+    description: 'Açiklama',
+    donate: 'Bağiş',
+    wechat: 'Wechat',
+    alipay: 'Alipay',
+    paypal: 'PayPal',
+    closeDonate: 'Bağişi Kapat',
+    feedback: 'Geri Bildirim',
+    close: 'Tekrar Gösterme',
+    scriptInstalling: 'Betik yükleniyor...',
+    search: 'Ara'
+  }
+}
