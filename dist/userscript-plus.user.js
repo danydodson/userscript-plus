95e49d22672660ed636d1f92eff013207d05c1ea
danydodson <danydodson@gmail.com>
2024-01-21 09:00:14 -0600

    :sparkles: refactors

--- dist/userscript-plus.user.js
+++ dist/userscript-plus.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name           UserScript: UserScripts Plus
+// @version        1.0.0
 // @description    Show all UserJS for current site.
 // @namespace      https://github.com/danydodson/userscript-plus
 // @author         Nonce
-// @version        2.4.0
 // @license        MIT
 // @icon           data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH3ggEBCQHM3fXsAAAAVdJREFUOMudkz2qwkAUhc/goBaGJBgUtBCZyj0ILkpwAW7Bws4yO3AHLiCtEFD8KVREkoiFxZzX5A2KGfN4F04zMN+ce+5c4LMUgDmANYBnrnV+plBSi+FwyHq9TgA2LQpvCiEiABwMBtzv95RSfoNEHy8DYBzHrNVqVEr9BWKcqNFoxF6vx3a7zc1mYyC73a4MogBg7vs+z+czO50OW60Wt9stK5UKp9Mpj8cjq9WqDTBHnjAdxzGQZrPJw+HA31oulzbAWgLoA0CWZVBKIY5jzGYzdLtdE9DlcrFNrY98zobqOA6TJKHW2jg4nU5sNBpFDp6mhVe5rsvVasUwDHm9Xqm15u12o+/7Hy0gD8KatOd5vN/v1FozTVN6nkchxFuI6hsAAIMg4OPxMJCXdtTbR7JJCMEgCJhlGUlyPB4XfumozInrupxMJpRSRtZlKoNYl+m/6/wDuWAjtPfsQuwAAAAASUVORK5CYII=
 // @include        *
 // @require        https://cdn.jsdelivr.net/gh/danydodson/l.js/userjs/l.userjs.min.js
 // @require        https://cdn.jsdelivr.net/gh/danydodson/l.js@master/lib.js
-// @resource       uiJs https://cdn.jsdelivr.net/gh/danydodson/Userscript-Plus/dist/ui.js?_=1705848913673
-// @resource       ui https://cdn.jsdelivr.net/gh/danydodson/userscript-plus/dist/ui.html?_=1705848913673
+// @resource       uiJs https://cdn.jsdelivr.net/gh/danydodson/Userscript-Plus/dist/ui.js?_=1705849184309
+// @resource       ui https://cdn.jsdelivr.net/gh/danydodson/userscript-plus/dist/ui.html?_=1705849184309
 // @resource       count https://greasyfork.org/scripts/by-site.json
 // @grant          GM_xmlhttpRequest
 // @grant          GM_getResourceText
